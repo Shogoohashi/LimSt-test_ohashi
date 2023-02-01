f121bd560d3760a6ebe959b78235a7fbd8f4dc28
Shogoohashi <76210851+Shogoohashi@users.noreply.github.com>
2023-02-01 23:27:04 +0900

    week3_sazaeのテストコードの命名を変更いたしました。

--- src/test/java/com/example/demo/week3/service/SazaeServiceTest.java
+++ src/test/java/com/example/demo/week3/service/SazaeServiceTest.java
@@ -9,21 +9,21 @@ class SazaeServiceTest {
     SazaeService service = new SazaeService();
 
     @Test
-    void janken1() {
+    void 出した手がグーの場合のテスト() {
         assertThat(service.janken("goo","goo"), is("draw"));
         assertThat(service.janken("goo","choki"), is("win"));
         assertThat(service.janken("goo","per"), is("lose"));
     }
 
     @Test
-    void janken2() {
+    void 出した手がパーの場合のテスト() {
         assertThat(service.janken("per","goo"), is("win"));
         assertThat(service.janken("per","choki"), is("lose"));
         assertThat(service.janken("per","per"), is("draw"));
     }
 
     @Test
-    void janken3() {
+    void 出した手がチョキの場合のテスト() {
         assertThat(service.janken("choki","goo"), is("lose"));
         assertThat(service.janken("choki","choki"), is("draw"));
         assertThat(service.janken("choki","per"), is("win"));
