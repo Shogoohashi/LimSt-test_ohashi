960a4f30baa66bbdfccf47982ed65f4cab9b98c1
Shogoohashi <76210851+Shogoohashi@users.noreply.github.com>
2023-02-01 23:20:52 +0900

    week3_nabaatuのテストコードの命名を変更いたしました。

--- src/test/java/com/example/demo/week3/service/SingleNabeatuServiceTest.java
+++ src/test/java/com/example/demo/week3/service/SingleNabeatuServiceTest.java
@@ -1,5 +1,6 @@
 package com.example.demo.week3.service;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
@@ -10,16 +11,19 @@ class SingleNabeatuServiceTest {
     SingleNabeatuService service = new SingleNabeatuService();
 
     @Test
+    @DisplayName("0だとエラーになる")
     void isAhoNumber1() {
         assertThat(service.isAhoNumber(0), is(false));
     }
 
     @Test
+    @DisplayName("３の倍数だとtrue")
     void isAhoNumber2() {
         assertThat(service.isAhoNumber(15), is(true));
     }
 
     @Test
+    @DisplayName("3の倍数以外だとエラーになる")
     void isAhoNumber3() {
         assertThat(service.isAhoNumber(2), is(false));
     }
