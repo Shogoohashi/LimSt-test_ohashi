63c42756e2cf065078723d55f31c046619e539b3
Shogoohashi <76210851+Shogoohashi@users.noreply.github.com>
2023-02-03 00:32:54 +0900

    途中

--- src/test/java/com/example/demo/week3/service/OmikujiServiceTest.java
+++ src/test/java/com/example/demo/week3/service/OmikujiServiceTest.java
@@ -21,9 +21,33 @@ class OmikujiServiceTest {
     }
 
     @Test
-    void getResultComment() {
+    void 凶の際のコメント() {
         String expected = "凶です、忘れ物など身の回りには要注意・・・";
         String actual = service.getResultComment("凶");
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    void 小吉の際のコメント() {
+        String expected = "小吉です、いつものように穏やかに過ごせるでしょう";
+        String actual = service.getResultComment("小吉");
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    void 中吉の際のコメント() {
+        String expected = "中吉です、いつも以上に楽しく過ごせるでしょう";
+        String actual = service.getResultComment("中吉");
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    void 大吉の際のコメント() {
+        String expected = "大吉です！きっと明日はいい天気になります。ラッキーカラーは緑です。" +
+                "大吉です！きっと明日はいい天気になります。ラッキーカラーは緑です。" +
+                "大吉です！きっと明日はいい天気になります。ラッキーカラーは緑です。" +
+                "大吉です！きっと明日はいい天気になります。ラッキーカラーは緑です。";
+        String actual = service.getResultComment("大吉");
         assertEquals(actual, expected);
     }
 }
