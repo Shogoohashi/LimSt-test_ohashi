4804ace9fd03cea407ab33733751cb2defa96f62
Shogoohashi <76210851+Shogoohashi@users.noreply.github.com>
2023-02-05 21:31:32 +0900

    コードを再修正致しました。

--- src/test/java/com/example/demo/week3/service/SazaeServiceTest.java
+++ src/test/java/com/example/demo/week3/service/SazaeServiceTest.java
@@ -1,54 +1,42 @@
 package com.example.demo.week3.service;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
-
+import org.junit.jupiter.api.Test;
 
 class SazaeServiceTest {
 
-
     SazaeService service = new SazaeService();
 
-    @ParameterizedTest
-    @CsvSource({"draw", "win", "lose"})
-    void 自分が出した手がグーの場合に勝つテスト(String expected) {
-        String actual = service.janken("goo", "choki");
-        if (actual.equals("win")) {
-            expected = "win";
-        } else if (actual.equals("draw")) {
-            expected = "draw";
-        } else {
-            expected = "lose";
-        }
-        assertEquals(actual, expected);
+    @Test
+    void 自分が出した手が勝つテスト() {
+        String expected = "win";
+        String actual1 = service.janken("goo", "choki");
+        String actual2 = service.janken("choki", "per");
+        String actual3 = service.janken("per", "goo");
+        assertEquals(actual1, expected);
+        assertEquals(actual2, expected);
+        assertEquals(actual3, expected);
     }
 
-    @ParameterizedTest
-    @CsvSource({"draw", "win", "lose"})
-    void 自分が出した手がチョキの場合に勝つテスト(String expected) {
-        String actual = service.janken("choki", "per");
-        if (actual.equals("win")) {
-            expected = "win";
-        } else if (actual.equals("draw")) {
-            expected = "draw";
-        } else {
-            expected = "lose";
-        }
-        assertEquals(actual, expected);
+    @Test
+    void 自分が出した手が引き分けテスト() {
+        String expected = "draw";
+        String actual1 = service.janken("goo", "goo");
+        String actual2 = service.janken("choki", "choki");
+        String actual3 = service.janken("per", "per");
+        assertEquals(actual1, expected);
+        assertEquals(actual2, expected);
+        assertEquals(actual3, expected);
     }
 
-    @ParameterizedTest
-    @CsvSource({"draw", "win", "lose"})
-    void 自分が出した手がパーの場合に勝つテスト(String expected) {
-        String actual = service.janken("per", "choki");
-        if (actual.equals("win")) {
-            expected = "win";
-        } else if (actual.equals("draw")) {
-            expected = "draw";
-        } else {
-            expected = "lose";
-        }
-        assertEquals(actual, expected);
+    @Test
+    void 自分が出した手が負けるテスト() {
+        String expected = "lose";
+        String actual1 = service.janken("per", "choki");
+        String actual2 = service.janken("goo", "per");
+        String actual3 = service.janken("choki", "goo");
+        assertEquals(actual1, expected);
+        assertEquals(actual2, expected);
+        assertEquals(actual3, expected);
     }
 }
