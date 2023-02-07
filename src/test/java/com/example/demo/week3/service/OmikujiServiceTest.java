d76820d1154e1c6c8ce763a072c306c6293c744c
Shogoohashi <76210851+Shogoohashi@users.noreply.github.com>
2023-02-07 22:45:55 +0900

    とりあえずコミット未修正

--- src/test/java/com/example/demo/week3/service/OmikujiServiceTest.java
+++ src/test/java/com/example/demo/week3/service/OmikujiServiceTest.java
@@ -1,57 +1,63 @@
 package com.example.demo.week3.service;
 
+
 import java.util.Random;
+import org.assertj.core.util.DateUtil;
 import org.junit.Before;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
-import static org.mockito.ArgumentMatchers.anyString;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
+import org.junit.runner.RunWith;
 import org.mockito.MockedConstruction;
-import static org.mockito.Mockito.doReturn;
+import org.mockito.Mockito;
 import static org.mockito.Mockito.mockConstruction;
-import static org.mockito.Mockito.when;
 import org.mockito.MockitoAnnotations;
+import static org.powermock.api.mockito.PowerMockito.doReturn;
+import static org.powermock.api.mockito.PowerMockito.mock;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
 
+
+@RunWith(PowerMockRunner.class)
+@PrepareForTest(DateUtil.class)
 class OmikujiServiceTest {
 
-    OmikujiService service = new OmikujiService();
-
-    @Mock
-    private OmikujiService mockInfo = new OmikujiService();
+    private OmikujiService omikujiService;
 
     @Before
     public void setup() {
-        MockitoAnnotations.initMocks(this);
+        MockitoAnnotations.openMocks(this);
     }
 
     @Test
     void おみくじ結果() {
         final String expected = "大吉";
-        when(mockInfo.getResult()).thenReturn("大吉");
-        String actual = service.getResult();
+        Random random = new Random();
+        Mockito.when(omikujiService).getClass();
+            OmikujiService service = new OmikujiService();
+            String actual = service.getResult();
             assertEquals(actual, expected);
+        }
 
     }
 
     @Test
     void 凶の際のコメント() {
         String expected = "凶です、忘れ物など身の回りには要注意・・・";
-        String actual = service.getResultComment("凶");
+        String actual = omikujiService.getResultComment("凶");
         assertEquals(actual, expected);
     }
 
     @Test
     void 小吉の際のコメント() {
         String expected = "小吉です、いつものように穏やかに過ごせるでしょう";
-        String actual = service.getResultComment("小吉");
+        String actual = omikujiService.getResultComment("小吉");
         assertEquals(actual, expected);
     }
 
     @Test
     void 中吉の際のコメント() {
         String expected = "中吉です、いつも以上に楽しく過ごせるでしょう";
-        String actual = service.getResultComment("中吉");
+        String actual = omikujiService.getResultComment("中吉");
         assertEquals(actual, expected);
     }
 
@@ -61,7 +67,7 @@ class OmikujiServiceTest {
                 "大吉です！きっと明日はいい天気になります。ラッキーカラーは緑です。" +
                 "大吉です！きっと明日はいい天気になります。ラッキーカラーは緑です。" +
                 "大吉です！きっと明日はいい天気になります。ラッキーカラーは緑です。";
-        String actual = service.getResultComment("大吉");
+        String actual = omikujiService.getResultComment("大吉");
         assertEquals(actual, expected);
     }
 }
