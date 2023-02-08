19cb02693017b47961e890ad0715b3469d450c9e
Shogoohashi <76210851+Shogoohashi@users.noreply.github.com>
2023-02-08 23:57:30 +0900

    テストケースを追加と桁数を追加、 テスト内容を変更いたしました。

--- src/test/java/com/example/demo/week3/service/SingleNabeatuServiceTest.java
+++ src/test/java/com/example/demo/week3/service/SingleNabeatuServiceTest.java
@@ -1,6 +1,6 @@
 package com.example.demo.week3.service;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
@@ -11,38 +11,54 @@ class SingleNabeatuServiceTest {
     SingleNabeatuService service = new SingleNabeatuService();
 
     @ParameterizedTest
-    @ValueSource(ints = {3, 6, 9})
+    @ValueSource(ints = {3, 6, 9, 15, 99, 213, 5692})
     @DisplayName("3の倍数だとtrue")
-    void isAhoNumber1(int Number) {
-        boolean expected = true;
-        if (Number % 3 == 0) {
-            expected = true;
+    void isAhoNumber1(int expected) {
+        if (expected % 3 == 0) {
+            boolean actual = service.isAhoNumber(3);
+            assertThat(actual).isTrue();
         }
-        boolean actual = service.isAhoNumber(3);
-        assertEquals(actual, expected);
     }
 
     @ParameterizedTest
-    @ValueSource(ints = {1, 2, 4, 5, 7, 8})
-    @DisplayName("3の倍数でない数値はfalse")
-    void isAhoNumber2(int Number) {
-        boolean expected = true;
-        if (Number % 3 != 0) {
-            expected = false;
+    @ValueSource(ints = {3, 6, 9, 66, 666, 2456})
+    @DisplayName("6の倍数だとtrue")
+    void isAhoNumber2(int expected) {
+        if (expected % 3 == 0) {
+            boolean actual = service.isAhoNumber(6);
+            assertThat(actual).isTrue();
         }
-        boolean actual = service.isAhoNumber(1);
-        assertEquals(actual, expected);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {3, 6, 9, 27, 81, 1233})
+    @DisplayName("9の倍数だとtrue")
+    void isAhoNumber3(int expected) {
+        if (expected % 3 == 0) {
+            boolean actual = service.isAhoNumber(9);
+            assertThat(actual).isTrue();
+        }
+    }
+
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 4, 5, 7, 8, 5531, 20591})
+    @DisplayName("3の倍数でない数値はfalse")
+    void isAhoNumber4(int expected) {
+        if (expected % 3 == 0) {
+            boolean actual = service.isAhoNumber(3);
+            assertThat(actual).isFalse();
+        }
     }
 
     @ParameterizedTest
     @ValueSource(ints = {0})
     @DisplayName("数値が0の場合はfalse")
-    void isAhoNumber3(int Number) {
-        boolean expected = true;
-        if (Number == 0) {
-            expected = false;
+    void isAhoNumber5(int expected) {
+        if (expected == 0) {
+            boolean actual = service.isAhoNumber(0);
+            assertThat(actual).isFalse();
+
         }
-        boolean actual = service.isAhoNumber(0);
-        assertEquals(actual, expected);
     }
 }
