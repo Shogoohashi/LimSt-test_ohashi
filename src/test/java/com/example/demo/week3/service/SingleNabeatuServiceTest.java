6123a044820fb2f24ea64258a14dbe80dad2daf4
Shogoohashi <76210851+Shogoohashi@users.noreply.github.com>
2023-02-12 23:53:24 +0900

    IF文の削除とメソッド名称を変更しました。

--- src/test/java/com/example/demo/week3/service/SingleNabeatuServiceTest.java
+++ src/test/java/com/example/demo/week3/service/SingleNabeatuServiceTest.java
@@ -11,54 +11,34 @@ class SingleNabeatuServiceTest {
     SingleNabeatuService service = new SingleNabeatuService();
 
     @ParameterizedTest
-    @ValueSource(ints = {3, 6, 9, 15, 99, 213, 5692})
-    @DisplayName("3の倍数だとtrue")
+    @ValueSource(ints = {3, 6, 9, 15, 99, 213, 5693})
+    @DisplayName("3の数字が含まれている場合、取得できる")
     void isAhoNumber1(int expected) {
-        if (expected % 3 == 0) {
-            boolean actual = service.isAhoNumber(3);
-            assertThat(actual).isTrue();
-        }
+        boolean actual = service.isAhoNumber(3);
+        assertThat(actual).isTrue();
     }
 
     @ParameterizedTest
-    @ValueSource(ints = {3, 6, 9, 66, 666, 2456})
-    @DisplayName("6の倍数だとtrue")
+    @ValueSource(ints = {3, 6, 9, 15, 99, 213, 5692})
+    @DisplayName("3の倍数の場合、取得できる")
     void isAhoNumber2(int expected) {
-        if (expected % 3 == 0) {
-            boolean actual = service.isAhoNumber(6);
-            assertThat(actual).isTrue();
-        }
+        boolean actual = service.isAhoNumber(6);
+        assertThat(actual).isTrue();
     }
 
     @ParameterizedTest
-    @ValueSource(ints = {3, 6, 9, 27, 81, 1233})
-    @DisplayName("9の倍数だとtrue")
-    void isAhoNumber3(int expected) {
-        if (expected % 3 == 0) {
-            boolean actual = service.isAhoNumber(9);
-            assertThat(actual).isTrue();
-        }
-    }
-
-
-    @ParameterizedTest
-    @ValueSource(ints = {1, 2, 4, 5, 7, 8, 5531, 20591})
-    @DisplayName("3の倍数でない数値はfalse")
+    @ValueSource(ints = {3, 6, 9, 15, 99, 213, 5692})
+    @DisplayName("３の倍数または３の数字が含まれていない場合、取得できる")
     void isAhoNumber4(int expected) {
-        if (expected % 3 == 0) {
-            boolean actual = service.isAhoNumber(3);
-            assertThat(actual).isFalse();
-        }
+        boolean actual = service.isAhoNumber(2);
+        assertThat(actual).isFalse();
     }
 
     @ParameterizedTest
     @ValueSource(ints = {0})
-    @DisplayName("数値が0の場合はfalse")
+    @DisplayName("数値が0の場合,取得できる")
     void isAhoNumber5(int expected) {
-        if (expected == 0) {
-            boolean actual = service.isAhoNumber(0);
-            assertThat(actual).isFalse();
-
-        }
+        boolean actual = service.isAhoNumber(0);
+        assertThat(actual).isFalse();
     }
 }
