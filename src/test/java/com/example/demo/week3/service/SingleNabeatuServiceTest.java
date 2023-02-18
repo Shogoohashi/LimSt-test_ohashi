af6c02d2b9f19512944df18fe6fd072757850d42
Shogoohashi <76210851+Shogoohashi@users.noreply.github.com>
2023-02-18 17:43:45 +0900

    名称の変更とテストケースを追加いたしました。

--- src/test/java/com/example/demo/week3/service/SingleNabeatuServiceTest.java
+++ src/test/java/com/example/demo/week3/service/SingleNabeatuServiceTest.java
@@ -11,34 +11,83 @@ class SingleNabeatuServiceTest {
     SingleNabeatuService service = new SingleNabeatuService();
 
     @ParameterizedTest
+    @ValueSource(ints = {0})
+    @DisplayName("数値が0の場合,取得できる")
+    void isAhoNumber0(int expected) {
+        boolean actual = service.isAhoNumber(0);
+        assertThat(actual).isFalse();
+    }
+
+
+    @ParameterizedTest
     @ValueSource(ints = {3, 6, 9, 15, 99, 213, 5693})
-    @DisplayName("3の数字が含まれている場合、取得できる")
+    @DisplayName("3の倍数または3が含めれていない場合、取得できない")
     void isAhoNumber1(int expected) {
+        boolean actual = service.isAhoNumber(1);
+        assertThat(actual).isFalse();
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {3, 6, 9, 15, 99, 213, 5693})
+    @DisplayName("3の倍数または3が含めれていない場合、取得できない")
+    void isAhoNumber2(int expected) {
+        boolean actual = service.isAhoNumber(2);
+        assertThat(actual).isFalse();
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {3, 6, 9, 15, 99, 213, 5693})
+    @DisplayName("3の倍数または3が含めれている場合、取得できる")
+    void isAhoNumber3(int expected) {
         boolean actual = service.isAhoNumber(3);
         assertThat(actual).isTrue();
     }
 
     @ParameterizedTest
     @ValueSource(ints = {3, 6, 9, 15, 99, 213, 5692})
-    @DisplayName("3の倍数の場合、取得できる")
-    void isAhoNumber2(int expected) {
+    @DisplayName("3の倍数または3が含めれていない場合、取得できない")
+    void isAhoNumber4(int expected) {
+        boolean actual = service.isAhoNumber(4);
+        assertThat(actual).isFalse();
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {3, 6, 9, 15, 99, 213, 5692})
+    @DisplayName("3の倍数または3が含めれていない場合、取得できない")
+    void isAhoNumber5(int expected) {
+        boolean actual = service.isAhoNumber(5);
+        assertThat(actual).isFalse();
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {3, 6, 9, 15, 99, 213, 5692})
+    @DisplayName("3の倍数または3が含めれている場合、取得できる")
+    void isAhoNumber6(int expected) {
         boolean actual = service.isAhoNumber(6);
         assertThat(actual).isTrue();
     }
 
     @ParameterizedTest
     @ValueSource(ints = {3, 6, 9, 15, 99, 213, 5692})
-    @DisplayName("３の倍数または３の数字が含まれていない場合、取得できる")
-    void isAhoNumber4(int expected) {
-        boolean actual = service.isAhoNumber(2);
+    @DisplayName("3の倍数または3が含めれていない場合、取得できない")
+    void isAhoNumber7(int expected) {
+        boolean actual = service.isAhoNumber(7);
         assertThat(actual).isFalse();
     }
 
     @ParameterizedTest
-    @ValueSource(ints = {0})
-    @DisplayName("数値が0の場合,取得できる")
-    void isAhoNumber5(int expected) {
-        boolean actual = service.isAhoNumber(0);
+    @ValueSource(ints = {3, 6, 9, 15, 99, 213, 5692})
+    @DisplayName("3の倍数または3が含めれていない場合、取得できない")
+    void isAhoNumber8(int expected) {
+        boolean actual = service.isAhoNumber(8);
         assertThat(actual).isFalse();
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {3, 6, 9, 15, 99, 213, 5692})
+    @DisplayName("3の倍数または3が含めれている場合、取得できる")
+    void isAhoNumber9(int expected) {
+        boolean actual = service.isAhoNumber(9);
+        assertThat(actual).isTrue();
     }
 }
