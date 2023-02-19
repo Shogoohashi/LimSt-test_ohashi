191947432a7b32a084cea8617b38042df2cf1c26
Shogoohashi <76210851+Shogoohashi@users.noreply.github.com>
2023-02-19 22:26:11 +0900

    @DisplayNameを変更いたしました

--- src/test/java/com/example/demo/week4/service/NabeatuServiceTest.java
+++ src/test/java/com/example/demo/week4/service/NabeatuServiceTest.java
@@ -9,7 +9,7 @@ class NabeatuServiceTest {
     NabeatuService service = new NabeatuService();
 
     @Test
-    @DisplayName("入力値が1の場合、１が取得する")
+    @DisplayName("入力値が1の場合、合計の１を返す")
     void getTotalNum() {
         int expected = 1;
         int actual = service.getTotalNum(1);
@@ -17,7 +17,7 @@ class NabeatuServiceTest {
     }
 
     @Test
-    @DisplayName("入力値が9の場合、45が取得する")
+    @DisplayName("入力値が9の場合、合計の45を返す")
     void getTotalNum1() {
         int expected = 45;
         int actual = service.getTotalNum(9);
@@ -25,7 +25,7 @@ class NabeatuServiceTest {
     }
 
     @Test
-    @DisplayName("入力値が100000の場合、705082704が取得する")
+    @DisplayName("入力値が100000の場合、合計の705082704を返す")
     void getTotalNum2() {
         int expected = 705082704;
         int actual = service.getTotalNum(100000);
@@ -34,7 +34,7 @@ class NabeatuServiceTest {
 
 
     @Test
-    @DisplayName("合計が0の場合,取得できる")
+    @DisplayName("合計が0の場合,falseを返す")
     void isAhoNumber0() {
         boolean actual = service.isAhoNumber(0);
         assertThat(actual).isFalse();
@@ -42,63 +42,63 @@ class NabeatuServiceTest {
 
 
     @Test
-    @DisplayName("合計が1の場合、取得できない")
+    @DisplayName("合計が1の場合、falseを返す")
     void isAhoNumber1() {
         boolean actual = service.isAhoNumber(1);
         assertThat(actual).isFalse();
     }
 
     @Test
-    @DisplayName("合計が2の場合、取得できない")
+    @DisplayName("合計が2の場合、falseを返す")
     void isAhoNumber2() {
         boolean actual = service.isAhoNumber(2);
         assertThat(actual).isFalse();
     }
 
     @Test
-    @DisplayName("合計が3の場合、取得できる")
+    @DisplayName("合計が3の場合、trueを返す")
     void isAhoNumber3() {
         boolean actual = service.isAhoNumber(3);
         assertThat(actual).isTrue();
     }
 
     @Test
-    @DisplayName("合計が4の場合、取得できない")
+    @DisplayName("合計が4の場合、falseを返す")
     void isAhoNumber4() {
         boolean actual = service.isAhoNumber(4);
         assertThat(actual).isFalse();
     }
 
     @Test
-    @DisplayName("合計が5の場合、取得できない")
+    @DisplayName("合計が5の場合、falseを返す")
     void isAhoNumber5() {
         boolean actual = service.isAhoNumber(5);
         assertThat(actual).isFalse();
     }
 
     @Test
-    @DisplayName("合計が6の場合、取得できる")
+    @DisplayName("合計が6の場合、trueを返す")
     void isAhoNumber6() {
         boolean actual = service.isAhoNumber(6);
         assertThat(actual).isTrue();
     }
 
     @Test
-    @DisplayName("合計が7の場合、取得できない")
+    @DisplayName("合計が7の場合、falseを返す")
     void isAhoNumber7() {
         boolean actual = service.isAhoNumber(7);
         assertThat(actual).isFalse();
     }
 
     @Test
-    @DisplayName("合計が8の場合、取得できない")
+    @DisplayName("合計が8の場合、falseを返す")
     void isAhoNumber8() {
         boolean actual = service.isAhoNumber(8);
         assertThat(actual).isFalse();
     }
 
     @Test
-    @DisplayName("合計が9の場合、取得できる")
+    @DisplayName("合計が9の場合、trueを返す")
     void isAhoNumber9() {
         boolean actual = service.isAhoNumber(9);
         assertThat(actual).isTrue();
