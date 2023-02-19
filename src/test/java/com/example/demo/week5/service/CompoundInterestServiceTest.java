7200b488d6ebec94d78a38b2b5f59f09270f473a
Shogoohashi <76210851+Shogoohashi@users.noreply.github.com>
2023-02-19 23:30:10 +0900

    テストケースを追加いたしました。

--- src/test/java/com/example/demo/week5/service/CompoundInterestServiceTest.java
+++ src/test/java/com/example/demo/week5/service/CompoundInterestServiceTest.java
@@ -1,6 +1,6 @@
 package com.example.demo.week5.service;
 
-import static org.assertj.core.internal.bytebuddy.matcher.ElementMatchers.is;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
@@ -9,10 +9,26 @@ class CompoundInterestServiceTest {
     CompoundInterestService compoundInterestService = new CompoundInterestService();
 
     @Test
+    @DisplayName("元金100、金利0.3、年利5の場合、複利計算結果が101.50902704052424になる")
     void getResult() {
         double expected = 101.50902704052424;
         double actual = compoundInterestService.getResult(100,0.3,5);
         assertThat(actual).isEqualTo(expected);
+    }
 
+    @Test
+    @DisplayName("元金4500、金利0.6、年利10の場合、複利計算結果が4777.407873582227になる")
+    void getResult1() {
+        double expected = 4777.407873582227;
+        double actual = compoundInterestService.getResult(4500,0.6,10);
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    @DisplayName("元金15000、金利2、年利3の場合、複利計算結果が15918.12になる")
+    void getResult2() {
+        double expected = 15918.12;
+        double actual = compoundInterestService.getResult(15000,2,3);
+        assertThat(actual).isEqualTo(expected);
     }
 }
