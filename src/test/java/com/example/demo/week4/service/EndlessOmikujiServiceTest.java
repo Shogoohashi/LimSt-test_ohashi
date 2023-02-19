2c8ee2715dedaf214c87a4c3f43013d01f9f0173
Shogoohashi <76210851+Shogoohashi@users.noreply.github.com>
2023-02-19 21:46:32 +0900

    無限おみくじのテストケースを作成。

--- src/test/java/com/example/demo/week4/service/EndlessOmikujiServiceTest.java
+++ src/test/java/com/example/demo/week4/service/EndlessOmikujiServiceTest.java
@@ -1,22 +1,21 @@
 package com.example.demo.week4.service;
 
 import java.util.Random;
-import org.junit.Before;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import static org.mockito.ArgumentMatchers.anyInt;
-import org.mockito.Mock;
 import org.mockito.MockedConstruction;
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.mockConstruction;
-import org.mockito.MockitoAnnotations;
 
 class EndlessOmikujiServiceTest {
 
     EndlessOmikujiService endlessOmikujiService = new EndlessOmikujiService();
 
     @Test
-    void おみくじの結果が凶になった場合に結果を取得() {
+    @DisplayName("おみくじの結果が凶になった場合,結果を取得")
+    void getResult() {
         try (MockedConstruction<Random> ignored = mockConstruction(Random.class,
                 (mock, ctx) -> doReturn(0).when(mock).nextInt(anyInt()))) {
             String actual = endlessOmikujiService.getResult();
@@ -24,7 +23,15 @@ class EndlessOmikujiServiceTest {
             assertEquals(expected, actual);
         }
     }
+
     @Test
+    @DisplayName("おみくじの結果が凶の場合,凶のコメントを取得")
     void getResultComment() {
+        try (MockedConstruction<Random> ignored = mockConstruction(Random.class,
+                (mock, ctx) -> doReturn(0).when(mock).nextInt(anyInt()))) {
+            String actual = endlessOmikujiService.getResultComment();
+            String expected = "凶です、今あなたの後ろに・・・";
+            assertEquals(expected, actual);
+        }
     }
 }
