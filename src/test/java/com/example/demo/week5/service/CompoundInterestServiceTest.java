1a2da51b6ffe63647be58ff50fc9b4309f63aa1c
Shogoohashi <76210851+Shogoohashi@users.noreply.github.com>
2023-02-21 00:02:43 +0900

    actualの値を一度変数に入れ直して修正しました。

--- src/test/java/com/example/demo/week5/service/CompoundInterestServiceTest.java
+++ src/test/java/com/example/demo/week5/service/CompoundInterestServiceTest.java
@@ -12,7 +12,10 @@ class CompoundInterestServiceTest {
     @DisplayName("元金100、金利0.3、年利5の場合、複利計算結果が101.50902704052424になる")
     void getResult() {
         double expected = 101.50902704052424;
-        double actual = compoundInterestService.getResult(100,0.3,5);
+        int principal = 100;
+        double rate = 0.3;
+        int years = 5;
+        double actual = compoundInterestService.getResult(principal,rate,years);
         assertThat(actual).isEqualTo(expected);
     }
 
@@ -20,7 +23,10 @@ class CompoundInterestServiceTest {
     @DisplayName("元金4500、金利0.6、年利10の場合、複利計算結果が4777.407873582227になる")
     void getResult1() {
         double expected = 4777.407873582227;
-        double actual = compoundInterestService.getResult(4500,0.6,10);
+        int principal = 4500;
+        double rate = 0.6;
+        int years = 10;
+        double actual = compoundInterestService.getResult(principal,rate,years);
         assertThat(actual).isEqualTo(expected);
     }
 
@@ -28,7 +34,10 @@ class CompoundInterestServiceTest {
     @DisplayName("元金15000、金利2、年利3の場合、複利計算結果が15918.12になる")
     void getResult2() {
         double expected = 15918.12;
-        double actual = compoundInterestService.getResult(15000,2,3);
+        int principal = 15000;
+        double rate = 2;
+        int years = 3;
+        double actual = compoundInterestService.getResult(principal,rate,years);
         assertThat(actual).isEqualTo(expected);
     }
 }
